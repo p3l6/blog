a03b6d903afee905b0fe85b9e0a7f7e22374e91c
Paul Landers <paul@thelanders.family>
2025-09-23 20:03:56 -0400

    Lint rakefile

--- _plugins/no_date.rb
+++ _plugins/no_date.rb
@@ -1,7 +1,9 @@
-class Jekyll::PostReader
+module Jekyll
   # Override posts file matcher to use the same as drafts: DATELESS_FILENAME_MATCHER
   # Date will be set in yaml front matter instead
-  def read_posts(dir)
-    read_publishable(dir, "_posts", Jekyll::Document::DATELESS_FILENAME_MATCHER)
+  class PostReader
+    def read_posts(dir)
+      read_publishable(dir, '_posts', Jekyll::Document::DATELESS_FILENAME_MATCHER)
+    end
   end
 end
